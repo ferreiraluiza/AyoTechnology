ad4a5eb8f9ff4b47c808567bd7e652f80c9fc3a6
João Victor <ra46791342838@acaonsfatima.org.br>
2024-09-06 01:29:23 -0300

    o outro n deu certo

--- AyoTechnology/src/main.jsx
+++ AyoTechnology/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Login from './pages/login.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
