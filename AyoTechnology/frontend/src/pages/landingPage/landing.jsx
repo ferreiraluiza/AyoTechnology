5a104dd4dc05c13eee17f90ff61163b532a49c70
João Victor <ra46791342838@acaonsfatima.org.br>
2024-10-07 15:28:32 -0300

    CODAR FRONTEND É MT CHATOOOOOO

--- AyoTechnology/frontend/src/pages/landingPage/landing.jsx
+++ AyoTechnology/frontend/src/pages/landingPage/landing.jsx
@@ -4,10 +4,10 @@ export default function Landing() {
       {/*primeira tela*/}
       <div
         className="h-screen bg-cover bg-center"
-        style={{ backgroundImage: `url(${"/assets/imagem1.svg"})` }}
+        style={{ backgroundImage: `url(${"/assets/image/imagem1.svg"})` }}
       >
         {/*cabeçalho*/}
-        <div className="text-zinc-100 flex justify-between mx-32">
+        <div className="text-zinc-100 flex justify-between mx-32 space-y-6 mb-32 ">
 
         <button className="relative inline-flex items-center px-6 py-2  border-2 border-transparent rounded-full bg-transparent group ">
             <p className=" font-montserrat text-2xl">Sobre nós</p>
@@ -47,13 +47,16 @@ export default function Landing() {
 
         {/*restante*/}
 
-        <div className="flex justify-center items-center flex-col text-zinc-50">
-          <h1 className="text-4xl font-bold font-montserrat">Bem vindo</h1>
+        <div className="flex justify-center flex-col text-zinc-100 space-y-20 mt-12">
+          
+          <h1 className="text-4xl font-bold font-montserrat text-center ">Bem vindo</h1>
 
-          <p>
+<div className=" inline-block  flex-col text-zinc-100 space-y-20 mt-12 ">
+          <p className="text-5xl ms-28">
             A AYO TECHNOLOGY está pronta para atender suas necessidades e
             entregar o melhor serviço possível!
           </p>
+          </div>
         </div>
       </div>
     </div>
