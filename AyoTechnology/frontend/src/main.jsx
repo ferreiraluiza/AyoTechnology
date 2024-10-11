56b43fbae6ef1dd1c5cc447a527fffce56719cd1
João Victor <ra46791342838@acaonsfatima.org.br>
2024-10-11 13:03:01 -0300

    30 min pra fzr 3 cards (⌐■_■)

--- AyoTechnology/frontend/src/main.jsx
+++ AyoTechnology/frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
+import Login from "./pages/login/login.jsx";
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+    <Routes>
+      <Route path="/" element = {<App/>}/>
+      <Route path="/login" element = {<Login/>}/>
+    </Routes>
+   </Router>
   </StrictMode>,
 )
