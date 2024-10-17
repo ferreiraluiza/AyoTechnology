2933c6b65defdd1471fdb3226cbad4e05e769cf5
joaoclink221 <ra46791342838@acaonsfatima.org.br>
2024-10-17 11:58:18 -0300

    mlk, fiz coisa pra caralho hj em

--- AyoTechnology/frontend/src/landing.jsx
+++ AyoTechnology/frontend/src/landing.jsx
@@ -1,13 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Button from "./components/button"
+import Header from "./components/header"
 const App = ()=> {
   return (
     <>
-    <h1>hello world</h1>
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Button className="mt-10" href="#login">
-        alguma coisa
-      </Button>
+      
+      <Header/>
       </div> 
     <ButtonGradient/>
     </>
