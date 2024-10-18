1effc540956bfcea433aa0e7957b374a7aa1673c
joaoclink221 <ra46791342838@acaonsfatima.org.br>
2024-10-18 11:59:35 -0300

    tô no cmc do Hero

--- AyoTechnology/frontend/src/components/section.jsx
+++ AyoTechnology/frontend/src/components/section.jsx
@@ -1,6 +1,6 @@
-import SectionSvg from '../assets/svg/SectionSvg'
+import SectionSvg from "../assets/svg/SectionSvg";
 
-const section = ({
+const Section = ({
   className,
   id,
   crosses,
@@ -10,18 +10,19 @@ const section = ({
 }) => {
   return (
     <div
-    id={id}
-      className={`relative ${
+      id={id}
+      className={`
+      relative 
+      ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        }`
-      }`}
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
     >
       {children}
 
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
       {crosses && (
         <>
@@ -30,11 +31,11 @@ const section = ({
               crossesOffset && crossesOffset
             } pointer-events-none lg:block xl:left-10 right-10`}
           />
-          <SectionSvg crossesOffset={crossesOffset}/>
+          <SectionSvg crossesOffset={crossesOffset} />
         </>
       )}
     </div>
   );
 };
 
-export default section;
+export default Section;
